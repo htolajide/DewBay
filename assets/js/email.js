994086d517c21adabc7b494c282e691df9844020
Hammed Taofeek <htolajide@yahoo.com>
2020-10-26 21:15:12 +0100

    message-alert

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,13 +1,17 @@
-function sendEmail() {
+function sendEmail(event) {
+    event.preventDefault();
     const name = document.getElementById('form_name').value
     const phone = document.getElementById('form_phone').value
     const email = document.getElementById('form_email').value
     const subject = document.getElementById('form_subject').value
     const message = document.getElementById('form_message').value
+    const submit_btn = document.getElementById('form_btn')
+    const alertSuccess = document.getElementById('success_box')
+    const alertError = document.getElementById('error_box')
     console.log(name + message)
     const request = {
         method: 'post',
-        url: 'https://aba-trader.herokuapp.com/api/v1/contact/message',
+        url: 'https://aba-trader.herokuapp.com/api/v1/contact/message/dewbay',
         data: {
             name: name,
             phone: phone,
@@ -16,7 +20,14 @@ function sendEmail() {
             message: message
         }
     }
+    submit_btn.textContent = 'Processing...'
     axios(request).then( response => {
-        alert(response.data.data.message)
+        alertSuccess.textContent = response.data.data
+        alertSuccess.style.display = 'block'
+        submit_btn.textContent = 'SEND MESSAGE'
+    }).catch( error => { 
+        alertError.textContent = error
+        alertError.style.display = 'block'
+        submit_btn.textContent = 'SEND MESSAGE'
     });
 }
