890e19cf13c3eb24f24e0356d2bb5e40cd9dddc7
Hammed Taofeek <htolajide@yahoo.com>
2020-10-26 01:11:55 +0100

    new-staff-added

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,21 +1,22 @@
 function sendEmail() {
-    const name = document.getElementsByName('name').value
-    const phone = document.getElementsByName('phone').value
-    const email = document.getElementsByName('email').value
-    const subject = document.getElementsByName('subject').value
-    const message = document.getElementsByName('message').value
-    Email.send({
-        Host: "mail.abiaplaza.com.ng",
-        Username : "taofeekhammed@abiaplaza.com.ng",
-        Password : "olajide4real",
-        To : 'taofeekhammed@abiaplaza.com.ng',
-        From : email,
-        Subject : subject,
-        Body : `${name}
-            ${phone}
-            ${message}`,
-    })
-    .then(function(message){
-        alert("mail sent successfully")
+    const name = document.getElementById('form_name').value
+    const phone = document.getElementById('form_phone').value
+    const email = document.getElementById('form_email').value
+    const subject = document.getElementById('form_subject').value
+    const message = document.getElementById('form_message').value
+    console.log(name + message)
+    const request = {
+        method: 'post',
+        url: 'https://aba-trader.herokuapp.com/api/v1/contact/message',
+        data: {
+            name: name,
+            phone: phone,
+            email: email,
+            subject: subject,
+            message: message
+        }
+    }
+    axios(request).then( response => {
+        alert(response.data.data.message)
     });
 }
