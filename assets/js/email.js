bb6cdc09609c7911a5debb07298172311d245030
Hammed Taofeek <htolajide@yahoo.com>
2020-10-28 11:26:06 +0100

    office-address-updated

--- assets/js/email.js
+++ assets/js/email.js
@@ -8,26 +8,39 @@ function sendEmail(event) {
     const submit_btn = document.getElementById('form_btn')
     const alertSuccess = document.getElementById('success_box')
     const alertError = document.getElementById('error_box')
-
-    const request = {
-        method: 'post',
-        url: 'https://aba-trader.herokuapp.com/api/v1/contact/message/dewbay',
-        data: {
-            name: name,
-            phone: phone,
-            email: email,
-            subject: subject,
-            message: message
+    const errors = []
+    let errorMessage = ''
+    if (name === '')  errors.push('Name can not be empty')
+    if (phone === '') errors.push('Phone can not be empty')
+    if (email === '') errors.push('Email is require')
+    if (subject === '') errors.push('Subject is required')
+    if (message === '') errors.push('Message is required')
+    if (errors.length > 0) {
+        errors.forEach(error => {
+            errorMessage += `${error} <br>`
+        })
+        alertError.innerHTML = errorMessage
+    }else{
+        const request = {
+            method: 'post',
+            url: 'https://aba-trader.herokuapp.com/api/v1/contact/message/dewbay',
+            data: {
+                name: name,
+                phone: phone,
+                email: email,
+                subject: subject,
+                message: message
+            }
         }
+        submit_btn.textContent = 'Please wait...'
+        axios(request).then( response => {
+            alertSuccess.textContent = response.data.data
+            alertSuccess.style.display = 'block'
+            submit_btn.textContent = 'SEND MESSAGE'
+        }).catch( error => { 
+            alertError.textContent = error
+            alertError.style.display = 'block'
+            submit_btn.textContent = 'SEND MESSAGE'
+        });
     }
-    submit_btn.textContent = 'Please wait...'
-    axios(request).then( response => {
-        alertSuccess.textContent = response.data.data
-        alertSuccess.style.display = 'block'
-        submit_btn.textContent = 'SEND MESSAGE'
-    }).catch( error => { 
-        alertError.textContent = error
-        alertError.style.display = 'block'
-        submit_btn.textContent = 'SEND MESSAGE'
-    });
 }
